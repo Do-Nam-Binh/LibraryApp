6cdf43e465f311052a3c94607e90a83897bf9ae5
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-08-11 17:27:05 +0700

    Add method to insert and remove books

--- scripts.js
+++ scripts.js
@@ -18,16 +18,36 @@ function displayBooks(){
     for(let i = 0; i < myLibrary.length; i++){
         const card = document.createElement("div");
         card.classList.add("card");
+        card.classList.add(i);
         const bookName = document.createElement("div");
         bookName.textContent = myLibrary[i].name;
         const bookAuthor = document.createElement("div");
         bookAuthor.textContent = myLibrary[i].author;
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.classList.add('deleteBtn');
+        deleteBtn.addEventListener('click', removeBook);
+        
         card.appendChild(bookName);
         card.appendChild(bookAuthor);
-    
+        card.appendChild(deleteBtn);
         content.appendChild(card);
     }
     
 }
+
+function handleSubmit(e) {
+    e.preventDefault();
+    addBook(this.bookName.value, this.bookAuthor.value);
+}
+
+function removeBook(e){
+    myLibrary.splice(parseInt(e.target.parentNode.classList.value[5]), 1);
+    displayBooks();
+}
+
+const addBookForm = document.querySelector('.addBookForm');
+addBookForm.addEventListener('submit',handleSubmit)
+
 
 const content = document.querySelector('.main-content');
