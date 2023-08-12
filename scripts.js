98f899e38142ccdd78419861b9fb5c5664238e99
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-08-12 10:54:12 +0700

    Finish basic functionality of library app

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 let myLibrary = [];
 
-function Book(name, author){
+function Book(name, author, status){
     this.name = name;
     this.author = author;
+    this.status = status;
+
+    this.toggleReadStatus= function(){
+        if(this.status == 'read'){
+            this.status = 'notRead';
+        }else{
+            this.status = 'read';
+        }
+    }
+
 }
 
-function addBook(name, author){
-    let book = new Book(name, author);
+function addBook(name, author, status){
+    let book = new Book(name, author, status);
     myLibrary.push(book);
     displayBooks();
 }
@@ -19,10 +29,22 @@ function displayBooks(){
         const card = document.createElement("div");
         card.classList.add("card");
         card.classList.add(i);
+
         const bookName = document.createElement("div");
         bookName.textContent = myLibrary[i].name;
+
         const bookAuthor = document.createElement("div");
         bookAuthor.textContent = myLibrary[i].author;
+
+        const bookStatus = document.createElement("button");
+        bookStatus.classList.add('statusBtn')
+        if(myLibrary[i].status == 'read'){
+            bookStatus.textContent = 'Read';
+        }else{
+            bookStatus.textContent = 'Not read';
+        }
+        bookStatus.addEventListener('click', changeStatus);
+        
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "X";
         deleteBtn.classList.add('deleteBtn');
@@ -30,6 +52,7 @@ function displayBooks(){
         
         card.appendChild(bookName);
         card.appendChild(bookAuthor);
+        card.appendChild(bookStatus);
         card.appendChild(deleteBtn);
         content.appendChild(card);
     }
@@ -38,12 +61,22 @@ function displayBooks(){
 
 function handleSubmit(e) {
     e.preventDefault();
-    addBook(this.bookName.value, this.bookAuthor.value);
+    addBook(this.bookName.value, this.bookAuthor.value, this.readStatus.value);
 }
 
 function removeBook(e){
     myLibrary.splice(parseInt(e.target.parentNode.classList.value[5]), 1);
     displayBooks();
+}
+
+function changeStatus(e){
+    if(this.textContent == 'Read'){
+        this.textContent = 'Not read';
+    }else{
+        this.textContent = 'Read';
+    }
+    console.log(e.target.parentNode.classList.value[5]);
+    myLibrary[parseInt(e.target.parentNode.classList.value[5])].toggleReadStatus();
 }
 
 const addBookForm = document.querySelector('.addBookForm');
